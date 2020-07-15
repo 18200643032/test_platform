aa46da6e1d22effb254f0fa126a312a06de142e1
zz <707308491@qq.com>
2020-07-15 11:05:07 +0800

    "修改数据库"

--- zz_platform/zz_platform/settings.py
+++ zz_platform/zz_platform/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'zz_platform.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'platform',
+        'USER': 'root',
+        'PASSWORD': '123456',
+        'HOST': '120.24.222.3',
+
     }
 }
 
