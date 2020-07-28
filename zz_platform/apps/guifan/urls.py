cceaf97cd3157f6596f968d3b5a076c16262c9ee
zz <707308491@qq.com>
2020-07-28 14:58:59 +0800

    "1"

--- zz_platform/apps/guifan/urls.py
+++ zz_platform/apps/guifan/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     re_path("guifan", views.guifan),
     re_path("download", views.file_download),
     re_path("upload", views.file_upload),
-    re_path("opencv",views.opencv)
+    re_path("opencv",views.opencv),
+    re_path("test_image",views.img_test)
 
 ]
